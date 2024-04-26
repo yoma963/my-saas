28fd38a3416bd63e28286e0cafc6dc1b2b2a2e0d
Bence Cseloczki <cseloczki.bence@gmail.com>
2024-04-26 12:28:24 +0200

    pdf renderer is done

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,4 +1,17 @@
-const PdfRenderer = () => {
+"use client"
+
+import { Document, Page, pdfjs } from "react-pdf"
+
+import "react-pdf/dist/Page/AnnotationLayer.css"
+import "react-pdf/dist/Page/TextLayer.css"
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
+interface PdfRendererProps {
+  url: string
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between">
@@ -6,6 +19,15 @@ const PdfRenderer = () => {
           top bar
         </div>
       </div>
+
+      <div className="flex-1 w-full max-h-screen">
+        <div>
+          <Document file={url} className="max-h-full">
+            <Page pageNumber={1} />
+          </Document>
+        </div>
+      </div>
+
     </div>
   )
 }
